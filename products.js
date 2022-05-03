1b233b5c39eec4c9e9e15e7981634607edd14193
Txny <irakozeyves9@gmail.com>
2022-05-03 02:04:00 +0200

    Better for loops

--- products.js
+++ products.js
@@ -10,12 +10,12 @@ const item = [
 // 1 . Filter and show the product that will be bought when you don't have much money I mean Cheap one
 min_price = item[0].price
 console.log(min_price)
-var cheapest_item
+let cheapest_item
 
-for (var i = 1; i < item.length; i++){
-    if (item[i].price < min_price){
-        min_price = item[i].price
-        cheapest_item = item[i]
+for (let i of item){
+    if (i.price < min_price){
+        min_price = i.price
+        cheapest_item = i
     }
 }
 
@@ -24,31 +24,31 @@ console.log("Cheapest item is: " + cheapest_item.name + " at price of: " + cheap
 // 2 . Filter and show the product that will be expensive in the array
 max_price = item[0].price
 console.log(max_price)
-var most_expensive_item
+let most_expensive_item
 
-for (var i = 1; i < item.length; i++){
-    if (item[i].price > max_price){
-        max_price = item[i].price
-        most_expensive_item = item[i]
+for (let i of item){
+    if (i.price > max_price){
+        max_price = i.price
+        most_expensive_item = i
     }
 }
 
 console.log("Most expensive item is: " + most_expensive_item.name + " at price of: " + most_expensive_item.price)
 
 // 3 . Calculate the full price of all product combined
-var total_price = 0
+let total_price = 0
 
-for (var i = 0; i < item.length; i++){
-    total_price += item[i].price
+for (let i of item){
+    total_price += i.price
 }
 
 console.log(total_price)
 
 // 4 . Calculate the full price of all product combined and remove product that are under the 10 dollar
 
-var new_total_price = 0
+let new_total_price = 0
 
-for (var i=0; i < item.length; i++){
+for (let i=0; i < item.length; i++){
     if (item[i].price < 10){
         item.splice(i, 1)
     }
